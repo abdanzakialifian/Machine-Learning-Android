4cf39e596c1bef0eacbb5c270d4fe97a6ac7a81a
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2024-02-26 06:47:18 +0700

    Add main activity functionality and add custom view activity

---
+++ app/src/main/java/com/learn/machinelearningandroid/customview/CustomViewActivity.kt
@@ -0,0 +1,12 @@
+package com.learn.machinelearningandroid.customview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.learn.machinelearningandroid.R
+
+class CustomViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_view)
+    }
+}
---
+++ app/src/main/java/com/learn/machinelearningandroid/main/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.learn.machinelearningandroid.main
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.learn.machinelearningandroid.customview.CustomViewActivity
+import com.learn.machinelearningandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.apply {
+            btnCustomView.setOnClickListener {
+                navigateToCustomViewActivity()
+            }
+        }
+    }
+
+    private fun navigateToCustomViewActivity() {
+        startActivity(Intent(this, CustomViewActivity::class.java))
+    }
+}
